574c0e31a5beec94193bd4e2329b0d4f281dab78
Rasmus Højmark Falden <rafa@itu.dk>
2022-09-02 13:23:30 +0200

    implemented leapYear functions with tests

--- HelloWorld.tests/ProgramTests.cs
+++ HelloWorld.tests/ProgramTests.cs
@@ -1,16 +1,72 @@
+using FluentAssertions;
+using System.Reflection;
 using Xunit;
+
 
 namespace HelloWorld.tests;
 
 public class ProgramTests
 {
     [Fact]
-    public void Main_prints_hello_world()
+    public void Main_input_2004_output_True()
     {
-        //Assign
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
 
-        //Act
+        // Act
+        var program = Assembly.Load(nameof(HelloWorld));
+        program.EntryPoint?.Invoke(null, new[] { new string[] { "2004" } });
 
-        //Assert
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("True");
+    }
+
+    [Fact]
+    public void Main_input_2003_output_False()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        var program = Assembly.Load(nameof(HelloWorld));
+        program.EntryPoint?.Invoke(null, new[] { new string[] { "2003" } });
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("False");
+    }
+
+    [Fact]
+    public void Main_input_2000_output_True()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        var program = Assembly.Load(nameof(HelloWorld));
+        program.EntryPoint?.Invoke(null, new[] { new string[] { "2000" } });
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("True");
+    }
+    [Fact]
+    public void Main_input_1900_output_False()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        var program = Assembly.Load(nameof(HelloWorld));
+        program.EntryPoint?.Invoke(null, new[] { new string[] { "1900" } });
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("False");
     }
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace HelloWorld;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        foreach (string arg in args)
+        {
+            if (int.TryParse(arg, out var val))
+            {
+                Console.WriteLine(leapYear(val));
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+
+
+    static bool leapYear(int year)
+    {
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
