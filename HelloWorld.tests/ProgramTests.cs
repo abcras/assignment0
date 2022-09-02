fa1377581a796bffc4f146cb858b21577c245da9
Rasmus Højmark Falden <rafa@itu.dk>
2022-09-02 12:41:34 +0200

    comments

---
+++ HelloWorld.tests/ProgramTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace HelloWorld.tests;
+
+public class ProgramTests
+{
+    [Fact]
+    public void Main_prints_hello_world()
+    {
+        //Assign
+
+        //Act
+
+        //Assert
+    }
+}
